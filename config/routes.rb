b03c86df3c92991b9e6ea705177e628d3aee0eb2
codeamac08 <codeamac08@gmail.com>
2016-08-01 20:32:38 -0500

    finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  # root 'static_pages#home'
   get 'static_pages/home'
 
   get 'static_pages/help'
@@ -57,5 +58,8 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-  root 'application#hello'
+  get  'static_pages/home'
+  get  'static_pages/help'
+  get  'static_pages/about'
+  # root 'application#hello'
 end
